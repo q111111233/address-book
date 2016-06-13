4f08a159df4dac402f99d6a1c909495660b46b61
bo zhao and Noah Ramey <bo@>
2016-06-13 14:11:45 -0700

    display data in table and add fields if needed

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,45 @@
 // BACK END //
-function Contact(first, last, email) {
+function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-  this.email = email;
+  this.emails = [];
 }
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
-
-
-
 
 // FRONT END //
 $(function() {
+  $("#add-email").click(function() {
+    $("#new-email").append('<div id="new-email">' +
+                            '<div class="new-email">' +
+                              '<div class="form-group">' +
+                                '<label for="new-email">Enter an email:</label>' +
+                                '<input type="text" id="new-email" class="form-control">' +
+                              '</div>' +
+                            '</div>' +
+                          '</div>');
+  $("#fields").append('<td>Another Email</td>');
+  });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("#first-name").val();
     var inputtedLastName = $("#last-name").val();
-    var inputtedEmail = $("#email").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedEmail);
+    $(".new-email").each(function() {
+      var inputtedEmail = $(this).find("#new-email").val();
+      newContact.emails.push(inputtedEmail)
+    });
 
     $("#show-contact").show();
 
-    $("#table").append("<tr><td><span class='first-name'>" + newContact.firstName + "</span></td><td><span class='last-name'>" + newContact.lastName + "</span></td><td><span class='email'>" + newContact.email + "</span></td></tr>")
-
+    $("#table").append("<tr class='contact-table'><td><span class='first-name'>" +
+                        newContact.firstName +
+                        "</span></td><td><span class='last-name'>" +
+                        newContact.lastName +
+                        "</span></td></tr>");
+    newContact.emails.forEach(function(email) {
+      $(".contact-table").last().append("<td>" + email + "</td>");
+    });
   });
 });
